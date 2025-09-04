7c8f87bc8e04c651cc4a2821258d0bda18acf871
jgmsgabriel <jgmsgabriel@gmail.com>
2025-09-04 07:50:46 -0300

    adicao funcao content

--- app.py
+++ app.py
@@ -76,5 +76,28 @@ def scrape_title():
         return jsonify({"error": "URL is required"}), 400
     return get_title(url)
 
+def get_content(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        headers = []
+        for hearder_tag in ['h1', 'h2', 'h3']:
+            for header in soup.find_all(hearder_tag):
+                headers.append(header.get_text(strip=True))
+        
+        paragaphs = [p.get_text(strip=True) for p in soup.find_all('p')]
+        return jsonify({"headers": headers, "paragraphs": paragaphs})
+    except Exception as e:
+        return jsonify({"error":str(e)}), 500
+
+@app.route('/scrape/content', methods=['GET'])
+@auth.login_required
+def scrape_content():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "URL is required"}), 400
+    return get_content(url)
+
 if __name__ == '__main__':
     app.run(debug=True)
